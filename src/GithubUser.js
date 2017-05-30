75512c7318b3b3ac0a0f7a6a25ffbc044d75ab58
Samuel Arenas <sarenas@purdue.edu>
2017-05-30 16:16:57 -0400

    style github user

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,6 +1,19 @@
 import React, {Component} from 'react'
+import './GithubUser.css'
 
 class GithubUser extends Component{
+
+    state={
+        user:{
+            avatar_url:'',
+            login:'',
+            follower:'',
+            following:'',
+            location:'',
+            html_url:'',
+        }
+    }
+
     constructor(props){
         super(props)
         this.fetchUserData()
@@ -9,13 +22,21 @@ class GithubUser extends Component{
     fetchUserData=()=>{
         fetch(`https://api.github.com/users/${this.props.match.params.username}`)
         .then(response=>response.json())
-        .then(user => console.log(user))
-        
+        .then(user => this.setState({user}))
     }
 
     render(){
+        const {user}=this.state
         return(
-            <h1>user: {this.props.match.params.username}</h1>
+            <div className="github-user">
+                <img src={user.avatar_url} alt="user"/>
+                <h2>{user.login}</h2>
+                <h3>followers: {user.follower}</h3>
+                <h3>following: {user.following}</h3>
+                <h3>locations: {user.location}</h3>
+                <a href={user.html_url} target="_" >Link to {user.login}'s profile</a>
+
+            </div>
         )
     }
 }
